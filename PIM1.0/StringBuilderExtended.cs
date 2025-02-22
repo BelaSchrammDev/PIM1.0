25a65e5cdba641c8ec85187e1b5560db6351b23b
BelaSchrammDev <belaschramm@aol.de>
2025-02-22 16:30:30 +0100

    update StringBuilderExtended.cs

--- PIM1.0/StringBuilderExtended.cs
+++ PIM1.0/StringBuilderExtended.cs
@@ -6,17 +6,10 @@ namespace IngameScript
     {
         public class StringBuilderExtended
         {
-            readonly StringBuilder sb;
-
+            private StringBuilder sb;
             public StringBuilderExtended(int size) { sb = new StringBuilder(size); }
-
             public StringBuilder GetSB() { return sb; }
-
-            public string GetString() { return sb.ToString(); }
-
             public override string ToString() { return sb.ToString(); }
-
-
             public void Trim()
             {
                 int wsFromBegin = 0;
@@ -34,72 +27,35 @@ namespace IngameScript
                 sb.Remove(0, wsFromBegin);
                 sb.Remove(sb.Length - wsFromEnd, wsFromEnd);
             }
-
-
             public void Substring(StringBuilderExtended targetSBX, int startindex, int lenght = -1)
             {
                 Substring(targetSBX.sb, startindex, lenght);
             }
-
-
             public void Substring(StringBuilder targetSB, int startindex, int lenght = -1)
             {
                 targetSB.Clear();
                 if (startindex > sb.Length) return;
                 int endindex;
-                if (lenght > 0) endindex = startindex + lenght;
+                if (lenght > 0) endindex = startindex + lenght - 1;
                 else endindex = sb.Length - 1;
                 for (int i = startindex; i <= endindex; i++) targetSB.Append(sb[i]);
             }
-
             public bool IsEmpty() { return sb.Length == 0; }
-
-            public bool Equal(string text) { if (sb.Length == text.Length && Contains(text)) return true; return false; }
-
-            public void Insert(int pos, string text) { sb.Insert(pos, text); }
-
-            public void Append(StringBuilderExtended sbx) { if (sbx == null) return; sb.Append(sbx.sb); }
-
             public void Append(string text) { sb.Append(text); }
-
             public void Append(char c) { sb.Append(c); }
-
             public void AppendLF(string text) { Append(text + "\n"); }
-
-            public void AppendLF(StringBuilderExtended sbx) { if (sbx == null) return; Append(sbx); Append('\n'); }
-
             public void AppendLFifNotEmpty(string text) { if (text.Length > 0) AppendLF(text); }
-
-            public void AppendLFifNotEmpty(StringBuilderExtended sbx) { if (sbx != null && !sbx.IsEmpty()) AppendLF(sbx); }
-
             public void Clear() { sb.Clear(); }
-
-
-            public void SetText(string text1, string text2 = "", string text3 = "", string text4 = "", string text5 = "")
-            {
-                Clear();
-                Append(text1);
-                if (text2 != "") Append(text2);
-                if (text3 != "") Append(text3);
-                if (text4 != "") Append(text4);
-                if (text5 != "") Append(text5);
-            }
-
-            public void SetText(StringBuilderExtended sbx) { Clear(); Append(sbx); }
-
+            public void SetText(string text) { Clear(); Append(text); }
             public void SetText(char c) { Clear(); Append(c); }
-
             public void ToLower() { for (int i = 0; i < sb.Length; i++) sb[i] = char.ToLower(sb[i]); }
-
             public bool Contains(string s) { return IndexOf(s) != -1; }
-
-
             public int IndexOf(string s)
             {
                 int sIndex = 0;
                 int contains = 0;
                 int findIndex = -1;
-                for (int i = 0; sIndex < s.Length && i < sb.Length && i + (s.Length - sIndex) < sb.Length; i++, sIndex += contains)
+                for (int i = 0; sIndex < s.Length && i < sb.Length; i++, sIndex += contains)
                 {
                     if (sb[i] == s[sIndex])
                     {
